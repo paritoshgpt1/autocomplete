8c4a865d448c505170cad10b712332b2cda055cb
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2015-02-27 21:04:22 +0530

    Using HTML View Engine

--- config/express.js
+++ config/express.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
 module.exports = function() {
-    var app = express();
-    app.set('views', './app/views');
-	app.set('view engine', 'ejs');
-    require('../app/routes/routes.js')(app);
-    return app;
+var app=express();
+app.set('views', __dirname + '/app/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+require('./app/routes/routes.js')(app);
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url, function(error) {
+	if (error) {
+        console.log(error);
+    }
+});
+
+return app;
 };
