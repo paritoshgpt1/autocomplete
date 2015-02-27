6bd32b9628d3ebeeb19ee18f2b92d1d945cb5838
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2015-02-27 21:01:53 +0530

    Updated Routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,26 +1,26 @@
-var Word = require('../models/words');
+// var express=require('express');
+// var db = require('../models/words');
 
-module.exports = function(app, server) {
-    app.get('/', function(req, res){
-    	res.render('index.html');
+module.exports = function(app) {
+    var db = require('../models/words');
+    // var express = require('express');
+    // var app = express();
+    console.log("inside routes")
+    app.get('/',function(req,res){
+        res.render('index.html');
     });
 
-    app.get('/search', function(req, res){
-
-    	var word = new RegExp(req.query.word, 'i');
-    	var query = Word.find({"def": word}).limit(20);
-
-    	query.exec(function(err, words) {
-    		if(!err)
-    		{
-    			res.send(words,'Content-Type: application/json', 200);
-    		}
-    		else
-    		{
-    			res.send(JSON.stringify(err),'Content-Type: application/json', 404);
-    		}
-    	});
-
-
+    app.get('/search',function(req,res){
+        var word = new RegExp(req.query.word, 'i');
+        var query = db.find({"def": word}).limit(20);
+        query.exec(function(err, words) {
+            if (err) throw err;
+            var data = [];
+            for(i=0;i<words.length;i++)
+            {
+                data.push(words[i]['def']);
+            }
+            res.end(JSON.stringify(data));
+        });
     });
 };
